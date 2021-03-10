39022a6f0bbe48056e783225147b50ed5416bf0c
siddiq <siddiq.aiub@gmail.com>
2021-03-11 01:54:22 +0600

    resolve allignment issues

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ const showOutput = (outputValues) => {
     console.log(outputValues);
     document.querySelector('.output-top').innerHTML = `
         <p class="text-justify"> 
-        Dear Mr/ Mrs. <strong> ${outputValues.name} </strong>When your Child will be at the Age of  <strong> ${outputValues.ageGoal}</strong> years You are to have <strong>${outputValues.fv1} Taka </strong>For the Event
+        Dear Mr/ Mrs. <strong> ${outputValues.name} </strong>When your Child will be at the Age of  <strong> ${outputValues.ageGoal}</strong> years You are to have <strong>${outputValues.fv1} Taka </strong>For the Event <br>
         The Inflation is considered at <strong>${outputValues.inflationPercent}%</strong> , And The return is Estimated to be <strong>${outputValues.estimatedPercent}%</strong>
         </p>
         `
